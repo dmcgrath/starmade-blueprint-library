58c6395f6dbf18b974d1ab28e6a25fc9b9aeb537
Dan McGrath <danmcg.au@gmail.com>
2016-01-14 23:42:07 -0800

    Flag all entities as old

--- starmade/blueprint.py
+++ starmade/blueprint.py
@@ -2,7 +2,7 @@
 
 from google.appengine.ext import blobstore, ndb
 
-SCHEMA_VERSION_CURRENT = 21
+SCHEMA_VERSION_CURRENT = 22
 
 class Blueprint(ndb.Model):
     """Datastore Entity for Blueprints"""
